8a96ea6e46eff759f1c950d73c098329fbef6ef2
Ivan O <ioparin@pm.me>
2022-11-24 19:20:50 +0400

    wrapping xcframework as swift package

--- Package.swift
+++ Package.swift
@@ -3,17 +3,17 @@
 import PackageDescription
 
 #warning("move binary artifact to private hosting")
-#warning("core-ios-spm is public due to a bug is SPM/Xcode that prevents authentication")
+#warning("it's public due to a bug is SPM/Xcode that prevents authentication")
 let package = Package(
-    name: "darwell-xpcore",
+    name: "ios-binahactivator",
     platforms: [
         .iOS(.v14)
     ],
     products: [
         .library(
-            name: "DarwellXPCore",
+            name: "BinahActivator",
             targets: [
-                "DarwellXpCore", "DarwellCore"
+                "BinahActivator"
             ]
         ),
     ],
@@ -21,18 +21,10 @@ let package = Package(
 		.package(url: "git@github.com:ivalx1s/swift-tensorflowlite-spm.git", from: "2.7.0"),
 	],
     targets: [
-		.target(
-			name: "DarwellXpCore",
-			dependencies: [
-				"DarwellCore",
-				.product(name: "TensorFlowLite", package: "swift-tensorflowlite-spm")
-			],
-			path: "Sources"
-		),
        .binaryTarget(
-            name: "DarwellCore",
-            url: "https://github.com/darwell-inc/monolith-ios-spm/releases/download/1.3.2/DarwellCore.xcframework.zip",
-            checksum: "e02b20d2cc74690396dfbbd688b747ef00d336d6c668da0da192aa7733954f25"
+            name: "BinahActivator",
+            url: "https://github.com/darwell-inc/ios-binahactivator-spm/releases/download/1.0.3/BinahActivator.xcframework.zip",
+            checksum: "36c66e44dc091cbdfcf448e00d83877bf8b120224883c33d05b7a8967f9be3bf"
         )
     ]
 )
